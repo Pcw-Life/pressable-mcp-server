b14ef461a802f33109872e507c8e551e40763162
Phillip Weitlauf <phillip@pcwprops.com>
2026-03-04 21:37:58 -0700

    initial push of project files

--- tools/account.js
+++ tools/account.js
@@ -22,6 +22,30 @@ const accountTools = [
         handler: async (args) => {
             return await api.get('/account/activity_logs', args);
         }
+    },
+    {
+        name: 'pressable_get_account_activity_log_actions',
+        description: 'Get a list of all available activity log actions for the account.',
+        inputSchema: { type: 'object', properties: {} },
+        handler: async () => {
+            return await api.get('/account/activity_logs/actions');
+        }
+    },
+    {
+        name: 'pressable_get_available_addons',
+        description: 'Get a list of available account add-ons.',
+        inputSchema: { type: 'object', properties: {} },
+        handler: async () => {
+            return await api.get('/account/add-ons/available');
+        }
+    },
+    {
+        name: 'pressable_get_account_addons',
+        description: 'Get a list of add-ons attached to your account.',
+        inputSchema: { type: 'object', properties: {} },
+        handler: async () => {
+            return await api.get('/account/add-ons');
+        }
     }
 ];
 
